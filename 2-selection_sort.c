9e5634e675e029c2c22e77171997da221a642559
Isaac <zuderg@gmail.com>
2021-09-18 16:33:15 -0700

    removed unneccesary print arrays

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,7 +5,7 @@
  * @size: size of array
  * Return: void
  */
-void selection_sort(int *array, size_t size)
+/*void selection_sort(int *array, size_t size)
 {
 unsigned int innerLoop, outerLoop, posSmallest;
 int smallestVal, n;
@@ -31,4 +31,35 @@ array[posSmallest] = n;
 print_array(array, size);
 }
 }
+}
+*/
+
+void selection_sort(int *array, size_t size)
+{
+   unsigned int outerLoop, innerLoop, minPos;
+
+    // One by one move boundary of unsorted subarray
+for (outerLoop = 0; outerLoop < size - 1; outerLoop++)
+{
+        // Find the minimum element in unsorted array
+        minPos = outerLoop;
+        for (innerLoop = outerLoop+1; innerLoop < size; innerLoop++)
+	{
+	if (array[innerLoop] < array[minPos])
+        minPos = innerLoop;
+	}
+        // Swap the found minimum element with the first element
+        if (minPos != outerLoop)
+	{
+	swapNums(&array[minPos], &array[outerLoop]);
+	print_array(array, size);
+    	}
+}
+}
+
+void swapNums(int *a, int *b)
+{
+    int swap = *a;
+    *a = *b;
+    *b = swap;
 }
