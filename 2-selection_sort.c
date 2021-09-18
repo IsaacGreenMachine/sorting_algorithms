69ffb6973d19d70533ba083f661f1c1c36fb2f26
Isaac <zuderg@gmail.com>
2021-09-18 16:36:29 -0700

    appeased betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,61 +5,37 @@
  * @size: size of array
  * Return: void
  */
-/*void selection_sort(int *array, size_t size)
+
+void selection_sort(int *array, size_t size)
 {
-unsigned int innerLoop, outerLoop, posSmallest;
-int smallestVal, n;
-if (array == NULL || size < 2)
-return;
-for (outerLoop = 0; outerLoop < size; outerLoop++)
+unsigned int outerLoop, innerLoop, minPos;
+
+for (outerLoop = 0; outerLoop < size - 1; outerLoop++)
 {
-smallestVal = array[outerLoop];
-posSmallest = outerLoop;
-for (innerLoop = outerLoop; innerLoop < size; innerLoop++)
+minPos = outerLoop;
+for (innerLoop = outerLoop + 1; innerLoop < size; innerLoop++)
 {
-if (array[innerLoop] < smallestVal)
-{
-smallestVal = array[innerLoop];
-posSmallest = innerLoop;
+if (array[innerLoop] < array[minPos])
+minPos = innerLoop;
 }
-}
-if (outerLoop < size - 1)
+if (minPos != outerLoop)
 {
-n = array[outerLoop];
-array[outerLoop] = smallestVal;
-array[posSmallest] = n;
+swapNums(&array[minPos], &array[outerLoop]);
 print_array(array, size);
 }
 }
 }
-*/
 
-void selection_sort(int *array, size_t size)
-{
-   unsigned int outerLoop, innerLoop, minPos;
-
-    // One by one move boundary of unsorted subarray
-for (outerLoop = 0; outerLoop < size - 1; outerLoop++)
-{
-        // Find the minimum element in unsorted array
-        minPos = outerLoop;
-        for (innerLoop = outerLoop+1; innerLoop < size; innerLoop++)
-	{
-	if (array[innerLoop] < array[minPos])
-        minPos = innerLoop;
-	}
-        // Swap the found minimum element with the first element
-        if (minPos != outerLoop)
-	{
-	swapNums(&array[minPos], &array[outerLoop]);
-	print_array(array, size);
-    	}
-}
-}
+/**
+ * swapNums - swaps two numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: void
+ */
 
 void swapNums(int *a, int *b)
 {
-    int swap = *a;
-    *a = *b;
-    *b = swap;
+int swap = *a;
+*a = *b;
+*b = swap;
 }
