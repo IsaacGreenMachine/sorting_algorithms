0c3a35680ccc29a02bb928646e634a68060fb0d3
Isaac <zuderg@gmail.com>
2021-09-18 16:07:53 -0700

    fringe cases and O(n) fixes

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,13 +9,12 @@ void selection_sort(int *array, size_t size)
 {
 unsigned int innerLoop, outerLoop, posSmallest;
 int smallestVal, n;
-if (array == NULL)
-return;
-if (size < 2)
+if (array == NULL || size < 2)
 return;
 for (outerLoop = 0; outerLoop < size; outerLoop++)
 {
 smallestVal = array[outerLoop];
+posSmallest = outerLoop;
 for (innerLoop = outerLoop; innerLoop < size; innerLoop++)
 {
 if (array[innerLoop] < smallestVal)
