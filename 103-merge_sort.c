991689a99878d6f725fbb56c2dfc09816f682e3b
Isaac <zuderg@gmail.com>
2022-02-07 20:44:44 +0000

    added sort and main

---
+++ 103-merge_sort.c
@@ -0,0 +1,92 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * CopyArray - copies array from one to another
+ * @A: array to be copied from
+ * @iBegin: beginning of array
+ * @iEnd: end of array
+ * @B: array to be copied to
+ * Return: Always void
+ */
+void CopyArray(int A[], int iBegin, int iEnd, int B[])
+{
+for (int k = iBegin; k < iEnd; k++)
+B[k] = A[k];
+}
+
+/*
+ * Left source half is A[ iBegin:iMiddle-1].
+ * Right source half is A[iMiddle:iEnd-1   ].
+ * Result is            B[ iBegin:iEnd-1   ].
+ */
+/**
+ * TopDownMerge - copies array from one to another
+ * @A: array to be copied from
+ * @iBegin: beginning of array
+ * @iMiddle: desc
+ * @iEnd: end of array
+ * @B: array to be copied to
+ * Return: Always void
+ */
+void TopDownMerge(int A[], int iBegin, int iMiddle, int iEnd, int B[])
+{
+int i = iBegin, j = iMiddle;
+
+/* While there are elements in the left or right runs... */
+for (int k = iBegin; k < iEnd; k++)
+{
+/* If left run head exists and is <= existing right run head. */
+if (i < iMiddle && (j >= iEnd || A[i] <= A[j]))
+{
+B[k] = A[i];
+i = i + 1;
+}
+else
+{
+B[k] = A[j];
+j = j + 1;
+}
+}
+}
+
+/*
+ * Split A[] into 2 runs, sort both runs into B[],
+ * merge both runs from B[] to A[]
+ * iBegin is inclusive; iEnd is exclusive (A[iEnd] is not in the set).
+ */
+/**
+ * TopDownSplitMerge - splits arrays using pointers and calls sorter function
+ * @B: dummy array
+ * @iBegin: beginning of array
+ * @iEnd: end of array
+ * @A: array with data
+ * Return: Always void
+ */
+void TopDownSplitMerge(int B[], int iBegin, int iEnd, int A[])
+{
+if (iEnd - iBegin <= 1)                     /* if run size == 1 */
+return;                                 /*   consider it sorted */
+/* split the run longer than 1 item into halves */
+int iMiddle = (iEnd + iBegin) / 2;              /* iMiddle = mid point */
+/* recursively sort both runs from array A[] into B[] */
+TopDownSplitMerge(A, iBegin,  iMiddle, B);  /* sort the left run */
+TopDownSplitMerge(A, iMiddle,    iEnd, B);  /* sort the right run */
+/* merge the resulting runs from array B[] into A[] */
+TopDownMerge(B, iBegin, iMiddle, iEnd, A);
+}
+
+/* Array A[] has the items to sort; array B[] is a work array. */
+/**
+ * merge_sort - final function for merge_sort
+ * @array: array to be sorted
+ * @size: size of array
+ * Return: Always void
+ */
+void merge_sort(int *array, size_t size)
+{
+int dummy[size];
+CopyArray(array, 0, size, dummy);           /* one time copy of A[] to B[] */
+TopDownSplitMerge(dummy, 0, size, array);   /* sort data from B[] into A[] */
+}
